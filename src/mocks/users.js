313cef20e70d3b9cfd23f65dca15e7d4da89f4ff
Pietra Costa <pietracosta.dev@gmail.com>
2025-10-24 15:58:24 -0300

    mocks

---
+++ src/mocks/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Pietra'
+    },
+     {
+        id: 2,
+        name: 'Enzo'
+    },
+     {
+        id: 3,
+        name: 'Bryan'
+    },
+    
+]
